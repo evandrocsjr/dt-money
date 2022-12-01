09bfd53029cc021c745c0ca4d6370c7bac166d79
evandrocsjr <evandrocunhajunior@gmail.com>
2022-11-30 21:11:54 -0300

    minor

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "../../components/Header";
+import { HeaderContainer } from "../../components/Header/styles";
+
+export function Transactions() {
+  return (
+    <HeaderContainer>
+      <Header />
+    </HeaderContainer>
+  );
+}
